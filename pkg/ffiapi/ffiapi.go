8c0e07508923f0810636a2ef2f9066fdda955478
Wilson Lin <code@wilsonl.in>
2020-07-20 17:42:46 +1000

    Use Go code instead of C for array access and allocation

--- pkg/ffiapi/ffiapi.go
+++ pkg/ffiapi/ffiapi.go
@@ -49,6 +49,10 @@ typedef struct ffiapi_loader {
 
 typedef void* (*allocator) (size_t bytes);
 
+static inline void* call_alloc(allocator alloc, size_t bytes) {
+	return alloc(bytes);
+}
+
 typedef void (*build_api_callback) (
 	void* cb_data,
 	ffiapi_output_file* output_files,
@@ -87,59 +91,6 @@ static inline ffiapi_string create_ffiapi_string(allocator alloc, _GoString_ gos
 	return create_ffiapi_string_from_bytes(alloc, len, godata);
 }
 
-//*** Create ffiapi_message array functions (interal Go use). ***
-
-static inline ffiapi_message* create_ffiapi_message_array(allocator alloc, size_t len) {
-	return alloc(sizeof(ffiapi_message) * len);
-}
-
-static inline void set_ffiapi_message_array_element(
-	ffiapi_message* array,
-	size_t i,
-	ffiapi_string file,
-	ptrdiff_t line,
-	ptrdiff_t column,
-	ptrdiff_t length,
-	ffiapi_string text
-) {
-	ffiapi_message msg = {
-		.file = file,
-		.line = line,
-		.column = column,
-		.length = length,
-		.text = text,
-	};
-	array[i] = msg;
-}
-
-//*** Create ffiapi_output_file array functions (interal Go use). ***
-
-static inline ffiapi_output_file* create_ffiapi_output_file_array(allocator alloc, size_t len) {
-	return alloc(sizeof(ffiapi_output_file) * len);
-}
-
-static inline void set_ffiapi_output_file_array_element(ffiapi_output_file* array, size_t i, ffiapi_string path, ffiapi_string data) {
-	ffiapi_output_file file = {
-		.path = path,
-		.data = data,
-	};
-	array[i] = file;
-}
-
-//*** Access input C array element functions (interal Go use). ***
-
-static inline ffiapi_define get_ffiapi_define_array_element(ffiapi_define* array, size_t i) {
-	return array[i];
-}
-
-static inline ffiapi_engine get_ffiapi_engine_array_element(ffiapi_engine* array, size_t i) {
-	return array[i];
-}
-
-static inline ffiapi_loader get_ffiapi_loader_array_element(ffiapi_loader* array, size_t i) {
-	return array[i];
-}
-
 //*** Call callback functions (interal Go use). ***
 
 static inline void call_build_api_callback(
@@ -174,7 +125,7 @@ import (
 	"unsafe"
 )
 
-func copyToCString(alloc C.allocator, bytes []byte) C.ffiapi_string {
+func toCString(alloc C.allocator, bytes []byte) C.ffiapi_string {
 	var godataPtr unsafe.Pointer = nil
 	if len(bytes) > 0 {
 		godataPtr = unsafe.Pointer(&bytes[0])
@@ -182,72 +133,69 @@ func copyToCString(alloc C.allocator, bytes []byte) C.ffiapi_string {
 	return C.create_ffiapi_string_from_bytes(alloc, C.size_t(len(bytes)), godataPtr)
 }
 
-func copyToCMessageArray(alloc C.allocator, messages []api.Message) (*C.ffiapi_message, C.size_t) {
+func toCMessageArray(alloc C.allocator, messages []api.Message) (*C.ffiapi_message, C.size_t) {
 	clen := C.size_t(len(messages))
-	carray := C.create_ffiapi_message_array(alloc, clen)
+	carray := (*C.ffiapi_message)(C.call_alloc(alloc, C.sizeof_ffiapi_message*clen))
+	carraySlice := (*[1 << 30]C.ffiapi_message)(unsafe.Pointer(carray))[:clen:clen]
 	for i, msg := range messages {
 		loc := msg.Location
 		if loc == nil {
 			loc = &api.Location{}
 		}
-		C.set_ffiapi_message_array_element(
-			carray,
-			C.size_t(i),
-			C.create_ffiapi_string(alloc, loc.File),
-			C.ptrdiff_t(loc.Line),
-			C.ptrdiff_t(loc.Column),
-			C.ptrdiff_t(loc.Length),
-			C.create_ffiapi_string(alloc, msg.Text),
-		)
+		carraySlice[i].file = C.create_ffiapi_string(alloc, loc.File)
+		carraySlice[i].line = C.ptrdiff_t(loc.Line)
+		carraySlice[i].column = C.ptrdiff_t(loc.Column)
+		carraySlice[i].length = C.ptrdiff_t(loc.Length)
+		carraySlice[i].text = C.create_ffiapi_string(alloc, msg.Text)
 	}
 	return carray, clen
 }
 
-func copyToCOutputFileArray(alloc C.allocator, outputFiles []api.OutputFile) (*C.ffiapi_output_file, C.size_t) {
+func toCOutputFileArray(alloc C.allocator, outputFiles []api.OutputFile) (*C.ffiapi_output_file, C.size_t) {
 	clen := C.size_t(len(outputFiles))
-	carray := C.create_ffiapi_output_file_array(alloc, clen)
+	carray := (*C.ffiapi_output_file)(C.call_alloc(alloc, C.sizeof_ffiapi_output_file*clen))
+	carraySlice := (*[1 << 30]C.ffiapi_output_file)(unsafe.Pointer(carray))[:clen:clen]
 	for i, file := range outputFiles {
-		C.set_ffiapi_output_file_array_element(
-			carray,
-			C.size_t(i),
-			C.create_ffiapi_string(alloc, file.Path),
-			copyToCString(alloc, file.Contents),
-		)
+		carraySlice[i].path = C.create_ffiapi_string(alloc, file.Path)
+		carraySlice[i].data = toCString(alloc, file.Contents)
 	}
 	return carray, clen
 }
 
-func convertCDefineArrayToMap(cptr *C.ffiapi_define, clen C.size_t) map[string]string {
-	godefinesLen := int(clen)
-	godefines := make(map[string]string, godefinesLen)
-	for i := 0; i < godefinesLen; i++ {
-		define := C.get_ffiapi_define_array_element(cptr, C.size_t(i))
-		godefines[define.from] = define.to
+func fromCDefineArray(cptr *C.ffiapi_define, clen C.size_t) map[string]string {
+	length := int(clen)
+	slice := (*[1 << 30]C.ffiapi_define)(unsafe.Pointer(cptr))[:length:length]
+	govalue := make(map[string]string, length)
+	for i := 0; i < length; i++ {
+		define := slice[i]
+		govalue[define.from] = define.to
 	}
-	return godefines
+	return govalue
 }
 
-func convertCEngineArrayToSlice(cptr *C.ffiapi_engine, clen C.size_t) []api.Engine {
-	goenginesLen := int(clen)
-	goengines := make([]api.Engine, goenginesLen)
-	for i := 0; i < goenginesLen; i++ {
-		engine := C.get_ffiapi_engine_array_element(cptr, C.size_t(i))
-		goengines[i] = api.Engine{
+func fromCEngineArray(cptr *C.ffiapi_engine, clen C.size_t) []api.Engine {
+	length := int(clen)
+	slice := (*[1 << 30]C.ffiapi_engine)(unsafe.Pointer(cptr))[:length:length]
+	govalue := make([]api.Engine, length)
+	for i := 0; i < length; i++ {
+		engine := slice[i]
+		govalue[i] = api.Engine{
 			Name:    api.EngineName(engine.name),
 			Version: engine.version,
 		}
 	}
-	return goengines
+	return govalue
 }
 
-func convertCLoaderArrayToMap(cptr *C.ffiapi_loader, clen C.size_t) map[string]api.Loader {
-	goloadersLen := int(clen)
-	goloaders := make(map[string]api.Loader, goloadersLen)
-	for i := 0; i < goloadersLen; i++ {
-		loader := C.get_ffiapi_loader_array_element(cptr, C.size_t(i))
-		goloaders[loader.name] = api.Loader(loader.loader)
+func fromCLoaderArray(cptr *C.ffiapi_loader, clen C.size_t) map[string]api.Loader {
+	length := int(clen)
+	slice := (*[1 << 30]C.ffiapi_loader)(unsafe.Pointer(cptr))[:length:length]
+	govalue := make(map[string]api.Loader, length)
+	for i := 0; i < length; i++ {
+		loader := slice[i]
+		govalue[loader.name] = api.Loader(loader.loader)
 	}
-	return goloaders
+	return govalue
 }
 
 func callBuildApi(
@@ -258,9 +206,9 @@ func callBuildApi(
 ) {
 	goresult := api.Build(buildOptions)
 
-	coutputFiles, coutputFilesLen := copyToCOutputFileArray(alloc, goresult.OutputFiles)
-	cerrors, cerrorsLen := copyToCMessageArray(alloc, goresult.Errors)
-	cwarnings, cwarningsLen := copyToCMessageArray(alloc, goresult.Warnings)
+	coutputFiles, coutputFilesLen := toCOutputFileArray(alloc, goresult.OutputFiles)
+	cerrors, cerrorsLen := toCMessageArray(alloc, goresult.Errors)
+	cwarnings, cwarningsLen := toCMessageArray(alloc, goresult.Warnings)
 
 	C.call_build_api_callback(cb, cbData, coutputFiles, coutputFilesLen, cerrors, cerrorsLen, cwarnings, cwarningsLen)
 }
@@ -308,7 +256,7 @@ func GoBuild(
 	go callBuildApi(alloc, cb, cbData, api.BuildOptions{
 		Sourcemap: api.SourceMap(sourceMap),
 		Target:    api.Target(target),
-		Engines:   convertCEngineArrayToSlice(engines, enginesLen),
+		Engines:   fromCEngineArray(engines, enginesLen),
 		Strict: api.StrictOptions{
 			NullishCoalescing: bool(strictNullishCoalescing),
 			ClassFields:       bool(strictClassFields),
@@ -321,7 +269,7 @@ func GoBuild(
 		JSXFactory:  jsxFactory,
 		JSXFragment: jsxFragment,
 
-		Defines:       convertCDefineArrayToMap(defines, definesLen),
+		Defines:       fromCDefineArray(defines, definesLen),
 		PureFunctions: pureFunctions,
 
 		GlobalName:        globalName,
@@ -333,7 +281,7 @@ func GoBuild(
 		Platform:          api.Platform(platform),
 		Format:            api.Format(format),
 		Externals:         externals,
-		Loaders:           convertCLoaderArrayToMap(loaders, loadersLen),
+		Loaders:           fromCLoaderArray(loaders, loadersLen),
 		ResolveExtensions: resolveExtensions,
 		Tsconfig:          tsconfig,
 
@@ -350,10 +298,10 @@ func callTransformApi(
 ) {
 	goresult := api.Transform(code, transformOptions)
 
-	cjs := copyToCString(alloc, goresult.JS)
-	cjsSourceMap := copyToCString(alloc, goresult.JSSourceMap)
-	cerrors, cerrorsLen := copyToCMessageArray(alloc, goresult.Errors)
-	cwarnings, cwarningsLen := copyToCMessageArray(alloc, goresult.Warnings)
+	cjs := toCString(alloc, goresult.JS)
+	cjsSourceMap := toCString(alloc, goresult.JSSourceMap)
+	cerrors, cerrorsLen := toCMessageArray(alloc, goresult.Errors)
+	cwarnings, cwarningsLen := toCMessageArray(alloc, goresult.Warnings)
 
 	C.call_transform_api_callback(cb, cbData, cjs, cjsSourceMap, cerrors, cerrorsLen, cwarnings, cwarningsLen)
 }
@@ -389,7 +337,7 @@ func GoTransform(
 	go callTransformApi(alloc, cb, cbData, code, api.TransformOptions{
 		Sourcemap: api.SourceMap(sourceMap),
 		Target:    api.Target(target),
-		Engines:   convertCEngineArrayToSlice(engines, enginesLen),
+		Engines:   fromCEngineArray(engines, enginesLen),
 		Strict: api.StrictOptions{
 			NullishCoalescing: bool(strictNullishCoalescing),
 			ClassFields:       bool(strictClassFields),
@@ -402,7 +350,7 @@ func GoTransform(
 		JSXFactory:  jsxFactory,
 		JSXFragment: jsxFragment,
 
-		Defines:       convertCDefineArrayToMap(defines, definesLen),
+		Defines:       fromCDefineArray(defines, definesLen),
 		PureFunctions: pureFunctions,
 
 		Sourcefile: sourceFile,
